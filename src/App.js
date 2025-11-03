0d35b9b3a883a62cb851a827541398a880cefa2c
AlexBer0-1 <alexberesnef@mail.ru>
2025-11-03 18:42:44 +0300

    Добавил фильтр великов

--- src/App.js
+++ src/App.js
@@ -1,25 +1,251 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Cart } from "./Cart";
+import {Card} from './components/Card';
+
+const Bikes = [
+  {
+    name: 'meteor',
+    price: 9999 ,
+    id: 1,
+    type: 'mountin',
+  },
+
+  {
+    name: 'starter',
+    price: 19999 ,
+    id: 2,
+    type: 'city',
+  },
+
+  {
+    name: 'goida',
+    price: 29999 ,
+    id: 3,
+    type: 'mountin',
+  },
+
+  {
+    name: 'sasha',
+    price: 39999 ,
+    id: 4,
+    type: 'city',
+  },
+]
+
+
 
 function App() {
+
+const [cart, setcart] = useState([])
+
+const [isCartOpen, setisCartOpen] = useState(false)
+
+  const [bikeType, setBikeType] = useState( 'all' )
+
+  const [showFilter, setShowFilter] = useState(false); 
+
+  const filteredBikes = Bikes.filter(bike  => bike.type === bikeType)
+
+  const handleDelete = (id) => {
+    const newCart = cart.filter(item => item.id != id)
+    setcart(newCart)  
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper  ">
+
+
+
+{isCartOpen && <Cart cart = {cart} onDelete =  {handleDelete}  > </Cart> } 
+    
+
+    
+
+
+
+      <header>
+    <div className="headerLeft">    
+      <img width={60} height={60} src  = "/img/logo.png" />
+      <div className="headerInfo">
+            <h3> React BIKE </h3>
+            <p>Луший магазин велосипедов</p> 
+          
+      </div>
+    </div>  
+      <ul className="headerRight">
+
+     <li> <img width={38} height={38} src  = "/img/cors.png" onClick={() => setisCartOpen(true) } />  <span> {cart.reduce((acc,item) => acc + item.price,0 ) } руб. </span> </li>
+     <li>
+      <img width={38} height={38} src  = "/img/unit.png" />
+     </li>
+
+
+      </ul>
+
       </header>
+      <div className="content" > 
+      <h1> Все велосипеды </h1>
+
+      <div className="search-block"> 
+        <img width={43} height={43} src="/img/Search.png" alt="Search"/>
+        <input placeholder="Поиск...."></input>
+
+      </div>
+
+      ...................... 
+
+      Кнопка фильтра 
+ <div className="filter">
+          <button
+            className="filterButton"
+            onClick={() => setShowFilter(!showFilter)}
+          >
+            Фильтр по типу
+          </button>
+
+          {showFilter && (
+            <div className="filterMenu">
+              <button onClick={() => setBikeType("all")}>Все</button>
+              <button onClick={() => setBikeType("mountin")}>Горные</button>
+              <button onClick={() => setBikeType("city")}>Городские</button>
+            </div>
+          )}
+        </div>
+
+
+
+...................
+      
+ <div className="bikes">
+
+{ filteredBikes.map((bike) => (
+   <div className="card">
+    <div className="favorite">
+    <img   width={25} height={25} src="/img/unlike.png" alt="unlike"/>
+    </div>
+         <img width={200} height={300} src  = "/img/bike1.png" /> 
+
+         {bike.name}
+         <div className="cardButtom">
+        <div>
+                  <span>  Цена: </span>
+                  {bike.price}
+            </div>
+            <button className="button" onClick={() => setcart([...cart,bike ]) }  >
+              <img src="ПЛЮС " alt="" />
+            </button>
+         </div>
+      </div>
+) )  }
+
+     
+      
+
+      
+
+      
+
+
+
+    
+      
+      
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+      </div>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Cart.js
@@ -0,0 +1,55 @@
+export const Cart = (props) => {
+    const {cart,onDelete} = props
+    return(
+        <div className="overlay">
+        <div className="drawer">
+          <h2>Корзина</h2>
+
+
+          {cart.map((bike) => { 
+            return( <div className="cartItem" >
+            <img width={60} height={60} src  = "/img/logo.png" />
+
+          <div>
+          <p> {bike.name}   </p>
+          <b> {bike.price}    </b>
+
+
+             </div>
+             <img className="removeButton" width={20} height={20} src  = "/img/krest.png" alt="крестик" onClick={() => onDelete(bike.id) } />
+          </div>
+
+            )
+          })}   
+          <div className="cartTotalBlock">
+           <ul >
+              <li>
+            <span> ИТОГО:  </span>
+
+            <div>  </div>
+            <b> 1000000000 </b>
+             </li>
+                <li>
+              <span>  Налог 8% </span>
+
+            <div>  </div>
+            <b> 10000%  </b>
+
+  
+                </li>
+
+
+
+           </ul>
+
+
+         
+
+
+       
+<button> Оформить заказ   </button>
+            </div>
+        </div>
+      </div>
+    )
+}
